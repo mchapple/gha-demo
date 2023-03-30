1a4c1261ab3972f73a77c3eae3215f0db95a8840
Matt Chapple <matt.chapple@ovo.com>
2023-03-30 16:06:19 +0100

    revised providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,22 @@ locals {
   bucket_name = "mgc-website-bucket"
 }
 
+terraform {
+  required_version = ">= 1.1.6"
+  backend "s3" {
+    bucket = "mgc-terra-state"
+    key    = "mgc-lovely-bucket"
+    region = "eu-west-1"
+  }
+
+  required_providers {
+    aws = {
+      #   source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
+}
+
 resource "aws_s3_bucket" "s3Bucket" {
   bucket = local.bucket_name
   acl    = "public-read"
