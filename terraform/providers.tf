3f3cd4a60816b60f49b0ea73978534e29aa7e419
Matt Chapple <matt.chapple@ovo.com>
2023-03-30 15:57:18 +0100

    provider update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,22 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "eu-west-1"
-}
-
 terraform {
+  required_version = ">= 1.1.6"
   backend "s3" {
     bucket = "mgc-terra-state"
     key    = "mgc-lovely-bucket"
     region = "eu-west-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
 }
+# terraform {
+#   backend "s3" {
+#     bucket = "mgc-terra-state"
+#     key    = "mgc-lovely-bucket"
+#     region = "eu-west-1"
+#   }
+# }
