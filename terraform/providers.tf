4c254f75ae966a16bdea44f005c4fab978eed448
Matt Chapple <matt.chapple@ovo.com>
2023-03-30 16:23:22 +0100

    re-reworked providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,22 +1,15 @@
-# terraform {
-#   required_version = ">= 1.1.6"
-#   backend "s3" {
-#     bucket = "mgc-terra-state"
-#     key    = "mgc-lovely-bucket"
-#     region = "eu-west-1"
-#   }
+terraform {
+  required_version = ">= 1.1.6"
+  backend "s3" {
+    bucket = "mgc-terra-state"
+    key    = "mgc-lovely-bucket"
+    region = "eu-west-1"
+  }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 3.0"
-#     }
-#   }
-# }
-# # terraform {
-# #   backend "s3" {
-# #     bucket = "mgc-terra-state"
-# #     key    = "mgc-lovely-bucket"
-# #     region = "eu-west-1"
-# #   }
-# # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+  }
+}
