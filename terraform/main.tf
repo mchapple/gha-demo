ff1eafe05d8cf7fb651c065e4110d6b983e47fe0
Matt Chapple <matt.chapple@sseenergyservices.com>
2023-03-31 15:37:15 +0100

    here we go again

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,6 @@
 locals {
   bucket_name = "mgc-other-website-bucket"
 }
-terraform {
-  required_version = ">= 1.3.8"
-  backend "s3" {
-    bucket = "mgc-terra-state"
-    key    = "mgc-lovely-bucket"
-    region = "eu-west-1"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_s3_bucket" "s3Bucket" {
   bucket = local.bucket_name
   acl    = "public-read"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,20 @@
 
+terraform {
+  required_version = ">= 1.3.8"
+  backend "s3" {
+    bucket = "mgc-terra-state"
+    key    = "mgc-lovely-bucket"
+    region = "eu-west-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
